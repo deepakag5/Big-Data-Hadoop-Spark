f0c0d2c570e03a5aa0465e39753d1d4ec6511ce2
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-23 21:51:10 -0500

    modified mapper file - Online Sales Mapper - added more tabs

--- Hadoop_MapReduce/OnlineSalesMapper.java
+++ Hadoop_MapReduce/OnlineSalesMapper.java
@@ -10,7 +10,8 @@ import org.apache.hadoop.mapred.*;
 public class SalesMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 	private final static IntWritable one = new IntWritable(1);
 
-	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+	throws IOException {
 
 		String valueString = value.toString();
 		String[] SingleCountryData = valueString.split(",");
